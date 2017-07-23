aeeb6db63ea48a9585e48fc743ca2a6173bfd3f1
Frank Carrasco <carrasco.frank10@gmail.com>
2017-07-23 16:44:04 -0300

    Criando layout de login e de validação.

---
+++ app/src/main/java/com/android/chatmessenger/chatmessenger/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.android.chatmessenger.chatmessenger.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.chatmessenger.chatmessenger.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
