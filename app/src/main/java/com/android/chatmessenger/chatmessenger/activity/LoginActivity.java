0b093b5755ada9ba7d2e657fe930f64e9f5a0177
Frank Carrasco <carrasco.frank10@gmail.com>
2017-07-24 20:33:55 -0300

    Criando mascara para telefone.

--- app/src/main/java/com/android/chatmessenger/chatmessenger/activity/LoginActivity.java
+++ app/src/main/java/com/android/chatmessenger/chatmessenger/activity/LoginActivity.java
@@ -2,14 +2,42 @@ package com.android.chatmessenger.chatmessenger.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.android.chatmessenger.chatmessenger.R;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextTelefone;
+    private EditText editTextDDI;
+    private EditText editTextDDD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextTelefone = (EditText) findViewById(R.id.editTextTel_Id);
+        editTextDDI = (EditText) findViewById(R.id.editTextDDI_Id);
+        editTextDDD = (EditText) findViewById(R.id.editTextDDD_Id);
+
+        //mascara telefone
+        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
+        MaskTextWatcher maskTextTelefone = new MaskTextWatcher(editTextTelefone, simpleMaskTelefone);
+        editTextTelefone.addTextChangedListener(maskTextTelefone);
+
+        //Mascara DDI
+        SimpleMaskFormatter simpleMaskDDI = new SimpleMaskFormatter("+NN");
+        MaskTextWatcher maskTextDDI = new MaskTextWatcher(editTextDDI, simpleMaskDDI);
+        editTextDDI.addTextChangedListener(maskTextDDI);
+
+        //Mascara DDD
+        SimpleMaskFormatter simpleMaskDDD = new SimpleMaskFormatter("NN");
+        MaskTextWatcher maskTextDDD = new MaskTextWatcher(editTextDDD, simpleMaskDDD);
+        editTextDDD.addTextChangedListener(maskTextDDD);
+
+
     }
 }
