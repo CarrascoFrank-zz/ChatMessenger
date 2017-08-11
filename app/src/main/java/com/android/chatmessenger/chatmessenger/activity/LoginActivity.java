44101e36a40394e196f295cc5e60948142cbbf59
Frank Carrasco <carrasco.frank10@gmail.com>
2017-08-10 22:46:34 -0300

    Login do usúario

--- app/src/main/java/com/android/chatmessenger/chatmessenger/activity/LoginActivity.java
+++ app/src/main/java/com/android/chatmessenger/chatmessenger/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,8 +19,15 @@ import com.android.chatmessenger.chatmessenger.R;
 import com.android.chatmessenger.chatmessenger.config.ConfiguracaoFirebase;
 import com.android.chatmessenger.chatmessenger.helper.Permissao;
 import com.android.chatmessenger.chatmessenger.helper.Preferencias;
+import com.android.chatmessenger.chatmessenger.model.Usuario;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.database.DatabaseReference;
 
 import java.util.HashMap;
@@ -30,24 +38,103 @@ public class LoginActivity extends AppCompatActivity {
 
     private DatabaseReference databaseReferenceFirebase;
 
+    private EditText editTextEmail;
+    private EditText editTextSenha;
+    private Button buttonLogar;
+    private Usuario usuario;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        verificarUsuarioLogado();
+
         databaseReferenceFirebase = ConfiguracaoFirebase.getFirebase();
 
+        editTextEmail = (EditText) findViewById(R.id.editText_email_id);
+        editTextSenha = (EditText) findViewById(R.id.editText_senha_id);
+        buttonLogar = (Button) findViewById(R.id.bt_logar_id);
+
+        //evento ao clicar em logar
+            buttonLogar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    usuario = new Usuario();
+                    usuario.setEmail(editTextEmail.getText().toString());
+                    usuario.setSenha(editTextSenha.getText().toString());
+
+                    validarLogin();
 
 
+                }
+            });
+
+
+    }
+
+        private void verificarUsuarioLogado(){
+            firebaseAuth = ConfiguracaoFirebase.getFirebaseAuth(); //instanciando o banco
+
+            //verificando se ele a esta logado.
+            if (firebaseAuth.getCurrentUser() != null){
+                abrirActivityPrincipal();
+            }
         }
-
 
         public void abrirCadastroUsuario(View view){
 
             Intent intent = new Intent(getApplicationContext(), CadastroUsuarioActivity.class);
             startActivity(intent);
+            finish();
+        }
+
+        private void abrirActivityPrincipal(){
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        private void validarLogin(){
+
+            firebaseAuth = ConfiguracaoFirebase.getFirebaseAuth();
+
+            firebaseAuth.signInWithEmailAndPassword(
+                    usuario.getEmail(),
+                    usuario.getSenha()
+            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+
+                        abrirActivityPrincipal();
+
+                    }else{
+
+                        String erroExcecao;
+
+                        try {
+                            throw task.getException();
+                        }catch (FirebaseAuthInvalidUserException e){
+                            erroExcecao = "E-mail invalidou ou nao existe";
+                        }catch (FirebaseAuthInvalidCredentialsException e){
+                            erroExcecao = "Senha Invalida, tente novamente";
+                        }catch (Exception e){
+                            erroExcecao = "Erro ao logar, tente novamente";
+                            e.printStackTrace();
+                        }
+
+                        Toast.makeText(getApplicationContext(), "ERRO: " + erroExcecao, Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
 
         }
+
+
 
 
 }
--- app/src/main/java/com/android/chatmessenger/chatmessenger/activity/MainActivity.java
+++ app/src/main/java/com/android/chatmessenger/chatmessenger/activity/MainActivity.java
@@ -1,22 +1,47 @@
 package com.android.chatmessenger.chatmessenger.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.chatmessenger.chatmessenger.R;
 import com.android.chatmessenger.chatmessenger.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button buttonSair;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonSair = (Button) findViewById(R.id.bt_sair_Id);
 
+        buttonSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                firebaseAuth = ConfiguracaoFirebase.getFirebaseAuth();
+                firebaseAuth.signOut();
+
+                abrirLoginActivity();
+
+            }
+        });
     }
+
+    private void abrirLoginActivity() {
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
