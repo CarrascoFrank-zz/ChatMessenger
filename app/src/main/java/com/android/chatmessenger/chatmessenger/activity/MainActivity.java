4fbb95eca26824a333afbd6f1ab2df80ce0d250e
Frank Carrasco <carrasco.frank10@gmail.com>
2017-08-20 15:00:48 -0300

    Criando a tela  de "Adcionando Contato", via alertDialog

--- app/src/main/java/com/android/chatmessenger/chatmessenger/activity/MainActivity.java
+++ app/src/main/java/com/android/chatmessenger/chatmessenger/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package com.android.chatmessenger.chatmessenger.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.android.chatmessenger.chatmessenger.Adapter.TabAdapter;
 import com.android.chatmessenger.chatmessenger.R;
@@ -83,11 +86,50 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             case R.id.actions_settings_id:
                 return true;
+            case R.id.item_adicionar_id:
+                abrirCadastroContato();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+
+    private void abrirCadastroContato(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+
+        //configurando o Dialog
+
+        alertDialog.setTitle("Novo Contato");
+        alertDialog.setMessage("E-mail do usuário");
+        alertDialog.setCancelable(false);
+
+        EditText editTextDialog = new EditText(MainActivity.this);
+
+        alertDialog.setView( editTextDialog );
+
+        //configurações dos botoes do Dialog
+
+        alertDialog.setPositiveButton("Adicionar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //evento ao adicionar
+
+            }
+        });
+
+
+        alertDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //evento ao cancelar
+
+            }
+        });
+
+        alertDialog.create();
+        alertDialog.show();
+    }
 
 
     private void deslogarUsuario(){
