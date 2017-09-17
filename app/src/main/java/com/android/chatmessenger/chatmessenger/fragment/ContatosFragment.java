3d1acfdf9c32b1796983f1377d167abcc2b097c1
Frank Carrasco <carrasco.frank10@gmail.com>
2017-09-16 22:24:58 -0300

    Finalizando listView de contatos.

--- app/src/main/java/com/android/chatmessenger/chatmessenger/fragment/ContatosFragment.java
+++ app/src/main/java/com/android/chatmessenger/chatmessenger/fragment/ContatosFragment.java
@@ -12,6 +12,13 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.android.chatmessenger.chatmessenger.R;
+import com.android.chatmessenger.chatmessenger.config.ConfiguracaoFirebase;
+import com.android.chatmessenger.chatmessenger.helper.Preferencias;
+import com.android.chatmessenger.chatmessenger.model.Contato;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +32,7 @@ public class ContatosFragment extends Fragment {
     private ListView listView;
     private ArrayAdapter adapter;
     private ArrayList<String> contatos;
+    private DatabaseReference databaseReference;
 
 
     public ContatosFragment() {
@@ -38,10 +46,6 @@ public class ContatosFragment extends Fragment {
 
         //Instanciando contatos
         contatos = new ArrayList<>();
-        contatos.add("Frank Carrasco");
-        contatos.add("Julia Costa");
-        contatos.add("Alejandra Carrasco");
-
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_contatos, container, false);
@@ -54,6 +58,39 @@ public class ContatosFragment extends Fragment {
         );
 
         listView.setAdapter(adapter);
+
+        //recuperando contatos no firebase]
+        Preferencias preferencias = new Preferencias(getActivity()); //instanciando as precferencias dentro da fragment
+        String identificadorUsuarioLogado = preferencias.getIdentificador(); //obtendo o indentificador do usuario atual logado.
+
+        databaseReference = ConfiguracaoFirebase.getFirebase()
+                .child("contatos")
+                .child(identificadorUsuarioLogado);
+
+        //listener para quando contatos for alterado.
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //Limpando a lista
+                contatos.clear();
+
+                //listando contatos
+                for(DataSnapshot dados: dataSnapshot.getChildren()){
+                    Contato contato = dados.getValue(Contato.class); //criando objeto baseado na classe contato
+                    contatos.add(contato.getNome());
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
         return view;
     }
 
